6f6fbd96901fa035d36306fb445cbabf58d12331
Pieter <pieter@vandeven.fr>
2023-11-11 16:30:26 +0100

    added env var

---
+++ config/scripts/extras.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Tell this script to exit if there are any errors.
+# You should have this in every custom script, to ensure that your completed
+# builds actually ran successfully without any errors!
+set -oue pipefail
+
+# Misc
+printf "MOZ_ENABLE_WAYLAND=1" >> /etc/environment
